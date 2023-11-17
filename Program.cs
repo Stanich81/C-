98cc5ae17985351584fc5ac0b4a2165857f0abb3
Stanislav <stanich81@gmail.com>
2023-11-18 01:24:05 +0300

    Поиск макс и мин из трёх чисел

--- Program.cs
+++ Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите 1е число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 2е число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите 3е число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max;
+int min;
+//поиск MAX
+if (a>b){
+    max = a;
+    }
+else{
+    max = b;
+}
+if (c>max){
+    max = c;
+}
+// поиск MIN
+if (a>b){
+    min = b;
+    }
+else{
+    min = a;
+}
+if (c<min){
+    min = c;
+}
+if(a==b && b==c){
+Console.Write($"Числа равны друг другу");    
+}
+else{
+    Console.Write($"min = {min}, max = {max}");
+}
